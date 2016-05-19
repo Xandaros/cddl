d0e63c28c4c20039e28f09db2f62a66aa8a320b9
Xandaros <mz.bremerhaven@gmail.com>
2016-05-19 19:14:57 +0200

    First draft of CDDL types

---
+++ src/Data/CDDL/Types.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE FlexibleInstances #-}
+module Data.CDDL.Types where
+
+import Data.List.NonEmpty
+import qualified Data.Map as Map
+import qualified Data.Text as Text
+import Data.Semigroup
+
+type Identifier = Text.Text
+
+type Typename = Identifier
+type Groupname = Identifier
+
+data CDDL = CDDL Typename Rules
+
+data Rules = Rules (Map.Map Typename Type) (Map.Map Groupname Group)
+-- Semigroup
+
+data Type = LiteralType Literal
+          | ChoiceType [Type] -- ∀n. Vector (S (S n)) Type
+          | RangeType Int Int Bool
+          | ListType Group
+          | StructType Group
+          | ReferencedType Typename
+          | RawType Int Int
+          | TaggedType Int Type
+          | AnyType
+        -- One possible Monoid instances - good idea?
+
+data Group = GroupProduct (NonEmpty Group)
+           | GroupChoice (NonEmpty Group)
+           | GroupReference Groupname
+        -- Two possible Monoid instances - no Monoid
+
+data Literal = NumberLiteral Double
+             | StringLiteral Text.Text
+
+
+-- TODO: Add special cases where grp1 or grp2 ere GroupChoice/GroupProduct
+instance Semigroup (Sum Group) where
+    (Sum grp1) <> (Sum grp2) = Sum $ GroupChoice (grp1 :| [grp2])
+
+instance Semigroup (Product Group) where
+    (Product grp1) <> (Product grp2) = Product $ GroupProduct (grp1 :| [grp2])
